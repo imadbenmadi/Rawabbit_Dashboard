4aed53f9895d80ebac6eb6f3e094666f87146869
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-25 17:30:22 +0100

    adding images and fix router

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,44 +3,44 @@ import { createBrowserRouter } from "react-router-dom";
 import Not_Finished from "./Components/Not_Finished";
 import Not_Found from "./Components/Not_Found";
 
-import Dashboard from "./Dashboard/Dashboard";
-import Dashboard_Login from "./Dashboard/Dashboard_Login";
-import Dashboard_home from "./Dashboard/Dashboard_home/Dashboard_home";
+import Dashboard from "./Dashboard";
+import Dashboard_Login from "./Dashboard_Login";
+import Dashboard_home from "./Components/Dashboard_home/Dashboard_home";
 
-import Dashboard_Users from "./Dashboard/Dashboard_Users/Dashboard_Users";
-import Default_user from "./Dashboard/Dashboard_Users/User/Default";
-import Edit_user from "./Dashboard/Dashboard_Users/User/Edit";
-import Dashboard_Users_Notification from "./Dashboard/Dashboard_Users/User/Notifications/Notifications";
-import Current_Notifications from "./Dashboard/Dashboard_Users/User/Notifications/Current_Notifications";
-import Add_user from "./Dashboard/Dashboard_Users/Add_user";
-import User from "./Dashboard/Dashboard_Users/User/User";
-import Table from "./Dashboard/Dashboard_Users/Table/Table";
-import Dashboard_User_Courses from "./Dashboard/Dashboard_Users/User/Courses/Courses";
-import Dashboard_User_Courses_Requests from "./Dashboard/Dashboard_Users/User/Courses_Requests/Courses_Requests";
-import Dashboard_User_Services from "./Dashboard/Dashboard_Users/User/Services/Services";
-import Dashboard_User_Services_Requests from "./Dashboard/Dashboard_Users/User/Services_Requests/Services_Requests";
+import Dashboard_Users from "./Components/Dashboard_Users/Dashboard_Users";
+import Default_user from "./Components/Dashboard_Users/User/Default";
+import Edit_user from "./Components/Dashboard_Users/User/Edit";
+import Dashboard_Users_Notification from "./Components/Dashboard_Users/User/Notifications/Notifications";
+import Current_Notifications from "./Components/Dashboard_Users/User/Notifications/Current_Notifications";
+import Add_user from "./Components/Dashboard_Users/Add_user";
+import User from "./Components/Dashboard_Users/User/User";
+import Table from "./Components/Dashboard_Users/Table/Table";
+import Dashboard_User_Courses from "./Components/Dashboard_Users/User/Courses/Courses";
+import Dashboard_User_Courses_Requests from "./Components/Dashboard_Users/User/Courses_Requests/Courses_Requests";
+import Dashboard_User_Services from "./Components/Dashboard_Users/User/Services/Services";
+import Dashboard_User_Services_Requests from "./Components/Dashboard_Users/User/Services_Requests/Services_Requests";
 
-import Dashboard_Courses from "./Dashboard/Dashboard_Courses/Dashboard_Courses";
-import Add_Course from "./Dashboard/Dashboard_Courses/Add_Course";
-import Current_Skate_Courses from "./Dashboard/Dashboard_Courses/Current_Courses";
-import Courses_Requests from "./Dashboard/Dashboard_Courses/Requests/Requests";
-import Edit_Course from "./Dashboard/Dashboard_Courses/Edit_Course";
+import Dashboard_Courses from "./Components/Dashboard_Courses/Dashboard_Courses";
+import Add_Course from "./Components/Dashboard_Courses/Add_Course";
+import Current_Skate_Courses from "./Components/Dashboard_Courses/Current_Courses";
+import Courses_Requests from "./Components/Dashboard_Courses/Requests/Requests";
+import Edit_Course from "./Components/Dashboard_Courses/Edit_Course";
 
-import Dashboard_Services from "./Dashboard/Dashboard_Services/Dashboard_Services";
-import Add_Service from "./Dashboard/Dashboard_Services/Add_Service";
-import Current_Skate_Services from "./Dashboard/Dashboard_Services/Current_Services";
-import Services_Requests from "./Dashboard/Dashboard_Services/Requests/Requests";
-import Edit_Service from "./Dashboard/Dashboard_Services/Edit_Service";
+import Dashboard_Services from "./Components/Dashboard_Services/Dashboard_Services";
+import Add_Service from "./Components/Dashboard_Services/Add_Service";
+import Current_Skate_Services from "./Components/Dashboard_Services/Current_Services";
+import Services_Requests from "./Components/Dashboard_Services/Requests/Requests";
+import Edit_Service from "./Components/Dashboard_Services/Edit_Service";
 
-import Dashboard_Events from "./Dashboard/Dashboard_Events/Dashboard_Events";
-import Add_Event from "./Dashboard/Dashboard_Events/Add_Event";
-import Current_Skate_Events from "./Dashboard/Dashboard_Events/Current_Events";
-import Edit_Event from "./Dashboard/Dashboard_Events/Edit_Event";
+import Dashboard_Events from "./Components/Dashboard_Events/Dashboard_Events";
+import Add_Event from "./Components/Dashboard_Events/Add_Event";
+import Current_Skate_Events from "./Components/Dashboard_Events/Current_Events";
+import Edit_Event from "./Components/Dashboard_Events/Edit_Event";
 
-import Dashboard_Blogs from "./Dashboard/Dashboard_Blogs/Dashboard_Blogs";
-import Add_Blog from "./Dashboard/Dashboard_Blogs/Add_Blog";
-import Current_Skate_Blogs from "./Dashboard/Dashboard_Blogs/Current_Blogs";
-import Edit_Blog from "./Dashboard/Dashboard_Blogs/Edit_Blog";
+import Dashboard_Blogs from "./Components/Dashboard_Blogs/Dashboard_Blogs";
+import Add_Blog from "./Components/Dashboard_Blogs/Add_Blog";
+import Current_Skate_Blogs from "./Components/Dashboard_Blogs/Current_Blogs";
+import Edit_Blog from "./Components/Dashboard_Blogs/Edit_Blog";
 
 const routes = createBrowserRouter([
     {
