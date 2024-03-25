0b2f82d984243325f55e2a56a49c96ce6387cd72
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-25 17:10:09 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import "./App.css";
+import Router from "./Router";
+import { RouterProvider } from "react-router";
+import { AppProvider } from "./Context/AppContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <AppProvider>
+            <RouterProvider router={Router} />
+        </AppProvider>
     </React.StrictMode>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                green2: "#10b981",
+                black_text: "#1f1f1f",
+                gray: "#525252",
+                gray_white: "#e6e6e6",
+                green: "#078343",
+                blue: "#004172",
+            },
+            fontFamily: {
+                customFont: ['"Outfit"', "sans-serif"],
+                // Add more custom font families as needed
+            },
+        },
     },
-    plugins: [],
+    plugins: [
+        require("tailwind-scrollbar")({
+            nocompatible: true,
+            preferredStrategy: "pseudoelements",
+        }),
+    ],
 };
