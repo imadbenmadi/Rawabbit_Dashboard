b3e70140e858ae34a4dcce65b3553a79d97e15e1
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-27 06:38:35 +0100

    fixing not found ui

--- src/Components/Dashboard_Users/User/WebSites/WebSites.jsx
+++ src/Components/Dashboard_Users/User/WebSites/WebSites.jsx
@@ -48,7 +48,7 @@ function WebSites() {
         }
     }
     return (
-        <>
+        <div className=" pt-6 ">
             <Link
                 to={`/Users/${userId}`}
                 className="select-none mb-4 w-fit m-auto bg-green rounded cursor-pointer text-white text-xl flex items-center gap-2 px-3 py-1 "
@@ -56,13 +56,13 @@ function WebSites() {
                 <IoMdArrowRoundBack />
                 <div>Back to user</div>
             </Link>
-            <div className=" flex text-gray items-center gap-2 pt-4 ">
+            <div className=" flex justify-center text-2xl text-gray items-center gap-2 pt-4  text-center m-auto">
                 <IoWarning className=" text-2xl" />
                 <div className="text-center text-gray">
                     No WebSite is Owned by this user
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
